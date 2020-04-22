edd197ae71d4f0ae2e924357c8516044e2d869b2
Eric Tran <e3tran@ucsd.edu>
2020-04-22 10:22:56 -0700

    set up react native env

---
+++ ios/Pods/Target Support Files/Pods-BitFit-BitFitTests/Pods-BitFit-BitFitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitFit_BitFitTests : NSObject
+@end
+@implementation PodsDummy_Pods_BitFit_BitFitTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-BitFit/Pods-BitFit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitFit : NSObject
+@end
+@implementation PodsDummy_Pods_BitFit
+@end
