7c67e7d7f9e594ca0a0116c8871544bef4fbd05b
Nour Yehia <noureldine.yehia@gmail.com>
2020-04-27 17:29:31 -0700

    Temp commit

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import Login from './src/Login';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Login);
