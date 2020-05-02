341cb004fb4db8ba85ea608dc37261c99a25ef9f
Samay Gandhi <sagandhi@ucsd.edu>
2020-05-03 01:23:32 +0530

    fixed the editing option

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React,{ Component, useState } from 'react';
 import {View, Text, TextInput, Image, StyleSheet, Button} from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
 
@@ -21,52 +21,50 @@ const styles = StyleSheet.create({
     marginBottom: 10
   }
 })
-function onPress(){
-  usernameActual=username
-  emailIdActual=emailId
-  nameActual=name
-}
+
 export default function Profile() {
   const [usernameActual, setUsernameActual] = useState('Username');
   const [emailIdActual, setEmailActual] = useState('Email@id.com');
   const [nameActual, setNameActual] = useState('Full Name');
-  const [username, setUsername] = useState('Enter Username');
-  const [emailId, setEmail] = useState('Enter email id');
-  const [name, setName] = useState('Enter full name');
+  const [username, setUsername] = useState(usernameActual);
+  const [emailId, setEmail] = useState(emailIdActual);
+  const [name, setName] = useState(nameActual);
   const [hrs, setHrs] = useState(20);
   const [sesh, setSesh] = useState(5);
-
 
   return (
     <View style={styles.profile_view}>
 
-    <Image style={styles.profile_photo}
+      <Image style={styles.profile_photo}
         source={require('../../images/profile.png')} alt='Profile Photo' />
-    <Text> Total Hrs: {hrs} </Text>
-    <Text> No. of sessions: {sesh} </Text>
-    <Button  
-    title="Click to Reset"
-    onPress={() => {
+      <Text> Total Hrs: {hrs} </Text>
+      <Text> No. of sessions: {sesh} </Text>
+      <Button  
+        title="Click to Reset"
+        onPress={() => {
         setHrs(0);
         setSesh(0);    
-    }}
-    />
-    <Text>{usernameActual}</Text>
-    <Text>{emailIdActual}</Text>
-    <Text>{nameActual}</Text>
-    <Button title="Click to Edit"/>
+      }}
+      />
+      <Text>{usernameActual}</Text>
+      <Text>{emailIdActual}</Text>
+      <Text>{nameActual}</Text>
+      <Button title="Click to Edit"/>
 
-      <TextInput
-        placeholder={username}
+      <TextInput 
+        placeholder={'Enter Username'}
         onChangeText={username => setUsername(username)}
+        defaultValue={username}
       />
       <TextInput
-        placeholder={name}
-        onChangeText={name => setName(name)}
-      />
-      <TextInput
-        placeholder={emailId}
+        placeholder={'Enter email id'}
         onChangeText={emailId => setEmail(emailId)}
+        defaultValue={emailId}
+      />
+      <TextInput
+        placeholder={'Enter full name'}
+        onChangeText={name => setName(name)}
+        defaultValue={name}
       />
       <Button 
         title="Click to Save" 
