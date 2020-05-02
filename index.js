815fe1923ff0613fc7262818aacf429459d0fb24
Eric Tran <e3tran@ucsd.edu>
2020-05-02 15:34:12 -0700

    override master with temp

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ import {AppRegistry} from 'react-native';
 import Login from './src/pages/Login';
 import {name as appName} from './app.json';
 
-
 AppRegistry.registerComponent(appName, () => Login);
-
