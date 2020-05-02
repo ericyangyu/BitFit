c8eed2f32b15ecb2c35c2713e52527468fbccc41
Eric Tran <e3tran@ucsd.edu>
2020-05-01 19:34:10 -0700

    updated timer branch, removed old timer branch

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Login from './src/pages/Login';
+// import Login from './src/pages/Login';
 import {name as appName} from './app.json';
+import Timer from './src/pages/Timer'
 
 
-AppRegistry.registerComponent(appName, () => Login);
+// AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => Timer);
 
