e5bad254fed8404ab9148c24b78df15d49c31ed7
Eric Tran <e3tran@ucsd.edu>
2020-05-03 16:35:15 -0700

    finalized workout pages front end

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import Login from './src/pages/Login';
 import {name as appName} from './app.json';
+import {SuggestedWorkouts} from './src/pages/Workouts'
 
-AppRegistry.registerComponent(appName, () => Login);
+// AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => SuggestedWorkouts);
