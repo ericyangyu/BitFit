a4deafce7f1d353e5763e22a0c276f55d5495c8f
Nour Yehia <noureldine.yehia@gmail.com>
2020-05-13 21:23:14 -0700

    Fixed typo

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,5 +3,5 @@ from .config import app
 # Import routes
 from .src.apis.user import user_api
 
-# Redgister blueprints
+# Register blueprints
 app.register_blueprint(user_api, url_prefix='/apis/user')
