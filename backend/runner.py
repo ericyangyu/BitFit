c5008246460433b8b462785e2a18894d38953cf3
Nour Yehia <noureldine.yehia@gmail.com>
2020-05-16 20:07:11 -0700

    Backend setup

--- backend/runner.py
+++ backend/runner.py
@@ -1,4 +1,4 @@
 from app.config import cli
 
 if __name__ == "__main__":
-    cli() //
+    cli()
--- frontend/src/style/c_text_field.js
+++
@@ -1,9 +0,0 @@
-import {StyleSheet} from 'react-native';
-
-export default StyleSheet.create({
-    textField: {
-        height: 30,
-        color: "#434343",
-        textAlign: "center"
-    }
-});
