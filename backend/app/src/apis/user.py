2694d626d8443e80ad726f0429e8680e7f745d3c
Nour Yehia <noureldine.yehia@gmail.com>
2020-05-16 23:45:51 -0700

    Backend setup

--- backend/app/src/apis/user.py
+++ backend/app/src/apis/user.py
@@ -1,50 +1,108 @@
-from flask import Blueprint, request, jsonify
+"""File to recieve API calls from the front about the user.
+
+Description: Recieves the intial REST API calls from the frontend related to
+             users.
+Handles the request data and only accepts POST requests.
+Calls the related backend user API calls to firebase.
+
+NOTE: Any parsing of the data should be handled here before calls are made to
+      the DB.
+
+Authors: Imran, Sharan, Nour
+"""
+
+# External imports #
+# import related flask packages
+from flask import Blueprint, request  # , jsonify
 from flask_cors import CORS
 
+#  Interal imports #######
+# import the User class
 from ..models.user import User
 
-user_api = Blueprint('user_api', __name__)
+# define the user_api blueprint route for all user related api calls
+user_api = Blueprint("user_api", __name__)
 CORS(user_api, supports_credentials=True)
 
 
-@user_api.route('/get_user', methods=['GET'])
-def get_user():
-    username = request.json['username']
-
-    u = User.get_user(username)
-
-    if not u:
-        return jsonify({'reason': 'User does not exist'}), 400
-    else:
-        return jsonify({'reason': 'User exists', 'result': u.to_json()}), 200
-
-
-@user_api.route('/create_user', methods=['POST'])
+@user_api.route("/create_user", methods=["POST"])
 def create_user():
-    username = request.json['username']
-    name = request.json['name']
-    email = request.json['email']
-    photo = request.json['photo']
+    """Creates a user based on provided email and password.
 
-    u = User.create_user(username, name, email, photo)
+    Returns:
+        Reponse object that contins data and a status code.
+    """
+    # read incoming request data body
+    email = request.json["email"]
+    password = request.json["password"]
+    fullname = request.json["fullname"]
+    username = request.json["username"]
+    avatar = request.json["avatar"]
 
-    if not u:
-        return jsonify({'reason': 'Username is not available'}), 400
-    else:
-        return jsonify({'reason': 'User created', 'result': u.to_json()}), 200
+    # make call to create user using firebase
+    response = User.create_user(email, password, fullname, username, avatar)
+
+    return response
 
 
-@user_api.route('/edit_user', methods=['PUT'])
-def edit_user():
-    username = request.json['username']
-    name = request.json['name'] if 'name' in request.json else None
-    email = request.json['email'] if 'email' in request.json else None
-    photo = request.json['photo'] if 'photo' in request.json else None
+@user_api.route("/login_user", methods=["POST"])
+def login_user():
+    """Logins in a user based on provided email and password.
 
-    u = User.get_user(username)
+    Returns:
+        Reponse object that contins data and a status code.
+    """
+    # read incoming request data body
+    email = request.json["email"]
+    password = request.json["password"]
+    # make call to login user using firebase
+    response = User.login_user(email, password)
+    return response
 
-    if not u:
-        return jsonify({'reason': 'User does not exist'}), 400
-    else:
-        u = u.edit_user(name, email, photo)
-        return jsonify({'reason': 'User edited', 'result': u.to_json()}), 200
+
+@user_api.route("/get_user", methods=["POST"])
+def get_user():
+    """Gets information about a user with specific UID.
+
+    Returns:
+        Reponse object that contins data about the user and a status code.
+    """
+    # read incoming request data body
+    UID = request.json["UID"]
+    # make call to get user data using firebase
+    response = User.get_user(UID)
+    return response
+
+
+@user_api.route("/update_user", methods=["POST"])
+def update_user():
+    """Updates data about a user with specific UID.
+
+    Returns:
+        Reponse object that contains only a status code.
+    """
+    # read incoming request data body
+    UID = request.json["UID"]
+    fullname = request.json["fullname"]
+    username = request.json["username"]
+    avatar = request.json["avatar"]
+    # make call to update user data using firebase
+    response = User.update_user(UID, fullname, username, avatar)
+
+    return response
+
+
+# NOTE: In progress, needs user idToken to work
+# @user_api.route("/delete_user", methods=["POST"])
+# def delete_user():
+# """Deletes a user with specific UID.
+
+# Returns:
+#     Reponse object that contains only a status code.
+# """
+#     # read incoming request data body
+#     UID = request.json["UID"]
+#     tokenId = request.json["tokenId"]
+#     # make call to delete user data using firebase
+#     response = User.delete_user(UID, tokenId)
+#     return response
--- backend/app/src/models/user.py
+++ backend/app/src/models/user.py
@@ -1,61 +1,219 @@
-from __future__ import annotations
-from typing import Dict
+"""
+File to make API calls to firebase and return reponse objects about a user.
 
-from ...config import db
+Description: Recieves data from the middleware, and makes the correct Pyrebase
+API calls to update data about the user in firebase. Contains error checking
+for errors in calls to Firebase.
+
+Authors: Imran, Sharan, Nour
+"""
+# External imports #
+# from __future__ import annotations
+# from typing import Dict
+
+# import package to load json
+import json
+
+# import flask related packages
+from flask import make_response, jsonify
+
+# import requests for api calls
+# import requests
+
+# import excpetion for HTTPErrors
+from requests.exceptions import HTTPError
+
+# IMPORTANT: Import sys and change the path to correctly import from config
+# import sys
+
+# Look above one line, changes path to import necessary firebase/pyrebase
+# variables
+# sys.path.append("../")
+from ...config import db, auth
 
 
-class User():
-    def __init__(self, username: str, name: str, email: str, photo: str):
-        self.username = username
-        self.name = name
-        self.email = email
-        self.photo = photo
+class User:
+    """
+    This class contains all of methods related to users in the firebase
+    database.
+    """
 
-    def to_json(self) -> Dict[str, str]:
-        to_return = {}
-        to_return['username'] = self.username
-        to_return['name'] = self.name
-        to_return['email'] = self.email
-        to_return['photo'] = self.photo
-        return to_return
+    def raise_detailed_error(self, request_object):
+        """
+        Used to raise an HTTPError if firebase calls are internally and not
+        through pyrebase.
 
-    def edit_user(self, name: str, email: str, photo: str) -> User:
-        if not db.child('users').child(self.username).get().val():
-            return None
+        Arguments:
+            request_object {[type]} -- Recieved from requests.post/get/...()
+                                       methods.
 
-        data = {}
-        if name is not None:
-            self.name = name
-            data['name'] = name
-        if email is not None:
-            self.email = email
-            data['email'] = email
-        if photo is not None:
-            self.photo = photo
-            data['photo'] = photo
+        Raises:
+            HTTPError
+        """
 
-        db.child('users').child(self.username).update(data)
-
-        return self
+        try:
+            request_object.raise_for_status()
+        except HTTPError as e:
+            # raise detailed error message
+            # TODO: Check if we get a { "error" : "Permission denied." } and
+            # handle automatically
+            raise HTTPError(e, request_object.text)
 
     @staticmethod
-    def get_user(username: str) -> User:
-        if not db.child('users').child(username).get().val():
-            return None
+    def create_error_message(self, e):
+        """
+        Handles exceptions in firebase calls, and returns correct error
+        repsonses.
 
-        name = db.child('users').child(username).child('name').get().val()
-        email = db.child('users').child(username).child('email').get().val()
-        photo = db.child('users').child(username).child('photo').get().val()
+        Arguments:
+            e {Exception} -- Wants an HTTPError exception, but can handle all
+                             excpetions.
 
-        return User(username, name, email, photo)
+        Returns:
+            A response object with an error code and no data
+        """
+        try:
+            # access the JSON object from error that contains JSON info
+            error_json = e.args[1]
+            # access message filed in JSON error object
+            code = json.loads(error_json)["error"]["code"]
+            return make_response({}, code)
+        except HTTPError:
+            return make_response({}, 400)
 
     @staticmethod
-    def create_user(username: str, name: str, email: str, photo: str) -> User:
-        if User.get_user(username) is not None:
-            return None
+    def create_user(
+        email: str, password: str, fullname: str, username: str, avatar: str
+    ):
+        """
+        Creates a user with an email and password. Also adds provided info into
+        database.
 
-        data = {'username': username, 'name': name, 'email': email,
-                'photo': photo}
-        db.child('users').child(username).set(data)
+        Arguments:
+            email {str} -- The user's email
+            password {str} -- The user's password
+            fullname {str} -- The user's fullname
+            username {str} -- The user's username
+            avatar {str} -- The user's avatar
 
-        return User(username, name, email, photo)
+        Returns:
+            Response object -- If valid call, returns the UID of the user and a
+            status code.
+        """
+        try:
+            # Create a user object through firebase call
+            user = auth.create_user_with_email_and_password(email, password)
+
+            # data to be added into DB for the user
+            data = {
+                "email": email,
+                "fullname": fullname,
+                "username": username,
+                "avatar": avatar,
+            }
+
+            # add data into the users's table with the user's localId as its
+            # key
+            # results = db.child("users").child(user["localId"]).set(data)
+
+            # return the user UID
+            data = jsonify({"UID": user["localId"]})
+            return make_response(data, 200)
+        except HTTPError as e:
+            # handle exception and return correct response object
+            return User.create_error_message(e)
+
+    @staticmethod
+    def login_user(email: str, password: str):
+        """
+        Logins in a user with an email and password.
+
+        Arguments:
+            email {str} -- The user's email
+            password {str} -- The user's password
+
+        Returns:
+            Response object -- If valid call, returns the UID of the user and a
+            status code.
+        """
+        try:
+            # sign in the user with firebase
+            user = auth.sign_in_with_email_and_password(email, password)
+            # return the user UID
+            data = jsonify({"UID": user["localId"]})
+            return make_response(data, 200)
+        except HTTPError as e:
+            # handle exception and return correct response object
+            return User.create_error_message(e)
+
+    @staticmethod
+    def get_user(UID: str):
+        """
+        Get's information about the user from the database.
+
+        Arguments:
+            UID {str} -- The user's unique id
+
+        Returns:
+            Response object -- If valid call, returns the user's data in the DB
+            and a status code.
+        """
+        try:
+            # get the data for the user in the users DB table and return it
+            results = db.child("users").child(UID).get().val()
+            return make_response(jsonify(results), 200)
+        except HTTPError as e:
+            # handle exception and return correct response object
+            return User.create_error_message(e)
+
+    @staticmethod
+    def update_user(UID: str, fullname: str, username: str, avatar: str):
+        """
+        Updates the user's information in the DB table users.
+
+        Arguments:
+            UID {str} -- The user's unique id
+            fullname {str} -- The user's new full name
+            username {str} -- The user's new username
+            avatar {str} -- The user's new avatar
+
+        Returns:
+            [type] -- [description]
+        """
+        try:
+            # the user's new data to update with
+            data = {
+                "fullname": fullname,
+                "username": username,
+                "avatar": avatar,
+            }
+            # updates the data in the db table users for this user and returns a status code
+            results = db.child("users").child(UID).update(data)
+            return make_response(results, 200)
+        except HTTPError as e:
+            # handle exception and return correct response object
+            return User.create_error_message(e)
+
+    # NOTE: In progress, needs user idToken to work
+    # @staticmethod
+    # def delete_user(UID: str):
+    #     try:
+    #         # remove the user from the database users table
+    #         results = db.child("users").child(UID).remove()
+
+    #         # remove the user from the firebase authentication table
+    #         request_ref = ("https://identitytoolkit.googleapis.com/v1/accounts:delete?key={0}".format(
+    #             auth.api_key
+    #         )
+    #         headers = {"content-type": "application/json; charset=UTF-8"}
+    #         data = json.dumps(
+    #             {
+    #                 "returnSecureToken": True,
+    #                 # "idToken": user["idToken"],
+    #             }
+    #         )
+    #         request_object = requests.post(request_ref, headers=headers, data=data)
+    #         # raise_detailed_error(request_object)
+    #         return make_response(jsonify(request_object.text), 200)
+    #     except HTTPError as e:
+    #         return create_error_response(e)
