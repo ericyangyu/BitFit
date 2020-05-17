e482efd25cb9c47a16e96b6c97928e059d1ffd21
Imran Matin <imranmatin23@gmail.com>
2020-05-17 11:39:58 -0700

    Commented the Routes.js page.

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,6 +1,14 @@
-// import { NativeRouter, Route, nativeHistory, Router} from "react-router-native";
+/**
+ * The Routes page is in charge of all routing for all pages on the app.
+ * 
+ * Authors: Imran, Sharan, Nour
+ */
+
+// library imports
 import React, { Component } from 'react';
 import { Router, Scene } from 'react-native-router-flux'
+
+// Page imports
 import Login from './Login'
 import SignUp from './SignUp'
 import Progress from './Progress'
@@ -8,21 +16,24 @@ import Profile from './Profile'
 import { MainFocus, SuggestedWorkouts } from './Workouts'
 import Timer from './Timer'
 
-class Routes extends React.Component {
-    render () {
+/**
+ * Class that returns a router object with all the pages.
+ */
+export default class Routes extends React.Component {
+    // Render the all the scenes for the router component
+    render() {
         return (
             <Router>
-                 <Scene key = "root">
-                    <Scene key = "login" component = {Login} initial = {true}  hideNavBar={true} />
-                    <Scene key = "signup" component = {SignUp} hideNavBar={true} />
-                    <Scene key = "progress" component = {Progress} hideNavBar={true} />
-                    <Scene key = "profile" component = {Profile} hideNavBar={true} />
-                    <Scene key = "mainfocus" component = {MainFocus} hideNavBar={true} />
-                    <Scene key = "suggestedworkouts" component = {SuggestedWorkouts} hideNavBar={true} />
-                    <Scene key = "timer" component = {Timer} hideNavBar={true} />
-                 </Scene>
+                <Scene key="root">
+                    <Scene key="login" component={Login} initial={true} hideNavBar={true} />
+                    <Scene key="signup" component={SignUp} hideNavBar={true} />
+                    <Scene key="progress" component={Progress} hideNavBar={true} />
+                    <Scene key="profile" component={Profile} hideNavBar={true} />
+                    <Scene key="mainfocus" component={MainFocus} hideNavBar={true} />
+                    <Scene key="suggestedworkouts" component={SuggestedWorkouts} hideNavBar={true} />
+                    <Scene key="timer" component={Timer} hideNavBar={true} />
+                </Scene>
             </Router>
         )
     }
 }
-export default Routes 
