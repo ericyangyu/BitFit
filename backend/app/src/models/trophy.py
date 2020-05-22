7501c11fbd04e23983fc4038efbc12780d221fed
Jeffrey Ha <jha@ucsd.edu>
2020-05-22 03:09:08 -0700

    refined api call for getting trophies

--- backend/app/src/models/trophy.py
+++ backend/app/src/models/trophy.py
@@ -26,66 +26,16 @@ class Trophy:
             Retrieves every single trophy in the trophy database.
 
             Returns:
-                A list of dictionaries containing the details of each trophy.
+                A dictionary in which the keys correspond to a trophy id and
+                its value corresponds to its actual trophy data.
         """
-        all_trophies = db.child("trophies").get()
-        trophies = []
+        trophies = {}
 
-        for data in all_trophies.each():
-            trophy_info = data.val()
-
-            trophies.append(trophy_info)
+        for data in db.child("trophies").get().each():
+            trophy = data.val()
+            trophies[trophy['id']] = trophy
 
         return trophies
-
-    @staticmethod
-    def get_trophy_by_id(self, trophy_id: int):
-        """
-            Retrieves a single trophy by its corresponding id.
-
-            Arguments:
-                trophy_id {int}: The trophy's unique id.
-
-            Returns:
-                A dictionary containing information about the trophy that
-                corresponds to the given id.
-
-                Returns None if not found.
-        """
-        trophies = self.get_all_trophies(self)
-
-        # get the first trophy that corresponds to the given trophy id
-        return next((x for x in trophies if x.id == trophy_id), None)
-
-    # @staticmethod
-    # def get_user_trophies(self, u_id: int):
-    #     """
-    #         Retrieves all of the user's trophies.
-
-    #         Arguments:
-    #             u_id {int}: The user's unique id.
-
-    #         Returns:
-    #             response_object -> If valid call, returns the list of user's
-    #             trophies and a 200 status code.
-    #     """
-    #     all_trophies = self.get_all_trophies()
-    #     user_trophies = []
-
-    #     # get all trophies earned..
-    #     all_trophies = db.child("earned_trophies").get()
-    #     for data in all_trophies.each():
-    #         trophy_info = data.val()
-
-    #         if trophy_info["user_id"] == u_id:
-    #             trophy_id = trophy_info["trophy_id"]
-    #             trophy_info["trophy_details"] = self.get_trophy_by_id(trophy_id)
-    #             user_trophies.append(trophy_info)
-
-    #     # question: do we need to handle the case where the user isn't found?
-    #     #           it doesn't seem to cause a problem b/c it'll just
-    #     #           return a list of empty trophies.
-    #     return make_response(user_trophies, 200)
 
     @staticmethod
     def get_user_trophies(self, uid: int):
@@ -99,5 +49,18 @@ class Trophy:
                 response_object -> If valid call, returns the list of user's
                 trophies and a 200 status code.
         """
-        results = db.child("trophies").get()
-        return make_response(results, 200)
+
+        all_trophies = self.get_all_trophies()
+
+        results = db.child("earned_trophies").order_by_child("user_id").equal_to(uid).get()
+        trophies = []
+
+        for troph in results:
+            data = troph.val()
+
+            # include another field that includes the trophy details
+            #   to avoid an extra API call on front-end
+            data['details'] = all_trophies[data['trophy_id']]
+            trophies.append(data)
+        
+        return make_response(trophies, 200)
