a0c19c74e988c5c11f627517983ff9616361d8d5
Nour Yehia <noureldine.yehia@gmail.com>
2020-05-28 18:49:15 -0700

    Updated api calls

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,6 +4,7 @@ import Constants from "expo-constants";
 const { manifest } = Constants;
 
 const api = axios.create({
+    // DO NOT CHANGE (this is how we connect to local host; thank u stack overflow)
   baseURL: `http://${manifest.debuggerHost.split(':').shift()}:4200/apis/`
 });
 
