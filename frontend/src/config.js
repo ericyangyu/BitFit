c5daa6bdc2c9f36a3d68519565cb413d8810209e
Nour Yehia <noureldine.yehia@gmail.com>
2020-05-28 18:49:44 -0700

    Updated api calls

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,7 +5,7 @@ const { manifest } = Constants;
 
 const api = axios.create({
     // DO NOT CHANGE (this is how we connect to local host; thank u stack overflow)
-  baseURL: `http://${manifest.debuggerHost.split(':').shift()}:4200/apis/`
+    baseURL: `http://${manifest.debuggerHost.split(':').shift()}:4200/apis/`
 });
 
 export default api;
